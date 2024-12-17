5017f43a02d1223af80c753a7362f7b78ac4f210
developer-tamim <tawsiftamim24@gmail.com>
2024-12-17 23:15:59 +0600

    lending page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,41 +1,47 @@
-import {FiFacebook, FiLinkedin, FiYoutube, FiInstagram} from "react-icons/fi"
-import {FaHandsClapping} from "react-icons/fa6"
-import Video from "../assets/bg.mp4"
+import { FiFacebook, FiLinkedin, FiYoutube, FiInstagram } from "react-icons/fi";
+import { FaHandsClapping } from "react-icons/fa6";
+import Video from "../assets/bg.mp4";
 
 const Landing = () => {
   return (
-    <div>
-        {/* Bg video */}
-        <video src={Video} autoPlay loop muted></video>
+    <div className="h-screen w-full flex justify-center items-center">
+      {/* Bg video */}
+      <video
+        src={Video}
+        autoPlay
+        loop
+        muted
+        className="absolute h-full w-full top-0 object-cover z-[-1]"
+      ></video>
 
-        {/* Social Media */}
-        <div>
-            <a href="#">
-                <FiFacebook/>
-            </a>
-            <a href="#">
-                <FiLinkedin/>
-            </a>
-            <a href="#">
-                <FiYoutube/>
-            </a>
-            <a href="#">
-                <FiInstagram/>
-            </a>
-        </div>
+      {/* Social Media */}
+      <div className="absolute left-3 flex flex-col gap-y-4 text-md text-gray-700">
+        <a href="#">
+          <FiFacebook />
+        </a>
+        <a href="#">
+          <FiLinkedin />
+        </a>
+        <a href="#">
+          <FiYoutube />
+        </a>
+        <a href="#">
+          <FiInstagram />
+        </a>
+      </div>
 
-        {/* Hero */}
-        <div>
-            <h1>
-                <span>
-                    Hello <FaHandsClapping/>
-                </span>
-                <span>I am a UI/UX Designer</span>
-                <button>Hire Me</button>
-            </h1>
-        </div>
+      {/* Hero */}
+      <div>
+        <h1 className="flex flex-col items-center gap-y-4 text-gray-700">
+          <span className="text-2xl flex items-center gap-x-3 font-normal">
+            Hello <FaHandsClapping />
+          </span>
+          <span className="text-5xl font-medium uppercase sm:text-3xl">I am a UI/UX Designer</span>
+          <button className="block w-24 bg-white mt-5 text-gray font-normal px-2 py-1 border border-gray-600 rounded-2xl hover:bg-gray-100 transition-colors">Hire Me</button>
+        </h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
