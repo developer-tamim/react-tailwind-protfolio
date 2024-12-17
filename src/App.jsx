796174c389265613d276b9fb1427fb4bed2b3cb5
developer-tamim <tawsiftamim24@gmail.com>
2024-12-17 23:20:41 +0600

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
+import About from "./components/About";
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Landing/>
+      <About/>
     </>
   )
 }
