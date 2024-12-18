50db9a9ad430dd01c350e9e3056d5a1e83fac84c
developer-tamim <tawsiftamim24@gmail.com>
2024-12-18 10:04:33 +0600

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,29 @@
+import myPic from "../assets/myPic.jpg"
+import CV from "../assets/nick-cv.pdf"
+import { FiFileText } from "react-icons/fi"
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      {/* Designer image*/}
+      <img src={myPic} alt="Designer" />
+
+      {/* About me text */}
+      <div>
+        <div>
+          <h2>I a UI/UX Designer</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe similique aperiam illo vel deleniti cum minima, adipisci consequatur laboriosam nobis. Explicabo incidunt dolore ut hic sequi quo aliquam soluta sapiente.
+          </p>
+        </div>
+
+        {/* Download CV */}
+        <a href="#">
+          <span>Download CV</span>
+          <FiFileText/>
+        </a>
+      </div>
+    </>
   )
 }
 
